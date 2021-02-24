c891161254dbc78a5f51448206d061a953ed543b
Russell Jewett <rfjewett3@gmail.com>
2021-02-24 17:10:40 -0500

    finishes server setup. Changes public folder structure for organization. Adds .env file for privacy.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const db = require("./models");
-const PORT = 8080;
+require("dotenv").config({ silent: true });
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Configuring the server
@@ -13,7 +14,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to MongoDB
-mongoose.connect();
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    UseFindandModify: false
+}, () => {
+    console.log("Mongoose is connected")
+});
 
-// Routes
-// 
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+});
