a50278187fec08c44117c5a88cd3261f63df9a4f
Russell Jewett <rfjewett3@gmail.com>
2021-02-28 15:54:22 -0500

    updates server to utilize Atlas. Adds route folder and api/html route files.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const db = require("./models");
-require("dotenv").config({ silent: true });
+
 const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+
+const db = require("./models");
 const app = express();
 
 // Configuring the server
@@ -14,12 +16,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to MongoDB
-mongoose.connect(process.env.ATLAS_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/FitnessTracker', {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     UseFindandModify: false
 }, () => {
     console.log("Mongoose is connected")
 });
+
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
