06803f14d3c2cca17c7b43322b7f55c1b7466326
Russell Jewett <rfjewett3@gmail.com>
2021-03-01 17:05:07 -0500

    adds html and api routes. range route still needs completion. App is working.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,41 @@
-// declare dependencies
+// Declare Dependencies
 const router = require("express").Router();
 const db = require("../models");
+const Workout = require("../models/workout");
+
+// All Workouts
+router.get("/api/workout", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+// New Exercise
+router.post("/api/workout", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+// Continue Exercise
+router.put("/api/workout/:id", (req, res) => {
+    Workout.updateOne({
+        _id: req.params.id
+    }, {
+        $push: { exercises: req.body }
+    })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
 
 
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,7 @@ router.get("/stats", (req, res) => {
 });
 
 // exercise
-router.get("/exercise", (req, res) => {
+router.get("/workout", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
