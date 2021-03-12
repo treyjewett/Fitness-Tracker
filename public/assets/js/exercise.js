f73e0d4b80526828f247dd3ca37bd07d8535ea6e
Russell Jewett <rfjewett3@gmail.com>
2021-03-12 11:51:59 -0500

    Adds favicon to all html pages.

--- public/assets/js/exercise.js
+++ public/assets/js/exercise.js
@@ -105,6 +105,7 @@ async function handleFormSubmit(event) {
     workoutData.name = cardioNameInput.value.trim();
     workoutData.distance = Number(distanceInput.value.trim());
     workoutData.duration = Number(durationInput.value.trim());
+    console.log(workoutData.name);
   } else if (workoutType === "resistance") {
     workoutData.type = "resistance";
     workoutData.name = nameInput.value.trim();
@@ -112,6 +113,7 @@ async function handleFormSubmit(event) {
     workoutData.sets = Number(setsInput.value.trim());
     workoutData.reps = Number(repsInput.value.trim());
     workoutData.duration = Number(resistanceDurationInput.value.trim());
+    console.log(workoutData.name);
   }
 
   await API.addExercise(workoutData);
